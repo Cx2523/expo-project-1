1a43f164d8b743723859add21b5f1b209422675c
Christopher Caldwell <clc523@gmail.com>
2018-08-07 07:51:18 -0600

    updated stopwatch component

--- components/StopWatch.js
+++ components/StopWatch.js
@@ -7,20 +7,25 @@ class StopWatch extends Component {
         super(props);
         this.state = {
             value: 0,
-            timer: null
+            timer: null,
+            timerIsRunning: false
         };
         this.tick = this.tick.bind(this);
         this.startTime = this.startTime.bind(this);
         this.stopTime = this.stopTime.bind(this);
-
+        this.pauseTime = this.pauseTime.bind(this);
+        this.msToTime = this.msToTime.bind(this);
     }
 
-    startTime() {
-        this.setState({timer: setInterval(this.tick,100)});
+    startTime() { 
+            this.setState({
+                timer: setInterval(this.tick,100),
+                timerIsRunning: true
+            });
     }
 
     tick(){
-        this.setState(prevState => {
+        this.setState(prevState => { 
             return {
                 value:  prevState.value + 100
             }
@@ -31,15 +36,55 @@ class StopWatch extends Component {
         clearInterval(this.state.timer);
         this.setState({
             timer: null,
-            value: 0
+            value: 0,
+            timerIsRunning: false
         });
+    }
+
+    pauseTime() {
+        clearInterval(this.state.timer);
+        this.setState({
+            timerIsRunning: false,
+            timer: null
+        });  
+    }
+
+    msToTime(duration) {
+        let milliseconds = parseInt((duration%1000)/100)
+        let seconds = parseInt((duration/1000)%60)
+        let minutes = parseInt((duration/(1000*60))%60)
+        let hours = parseInt((duration/(1000*60*60))%24);
+    
+        hours = (hours < 10) ? "0" + hours : hours;
+        minutes = (minutes < 10) ? "0" + minutes : minutes;
+        seconds = (seconds < 10) ? "0" + seconds : seconds;
+    
+        return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
     }
 
     render(){
         return (  
             <Card>
+                <H1 
+                    style={{
+                        textAlign: 'center',
+                        marginTop: '3%'
+                    }}
+                >Time</H1>
                 <View flexDirection={'row'} justifyContent={'space-around'}>
-                    <Text style={{fontSize: 50}}>{this.state.value}</Text>
+                    <Text style={{fontSize: 50, textAlign: 'center'}}>{this.msToTime(this.state.value)}</Text>
+                </View>
+                <View flexDirection={'row'} justifyContent={'space-around'}>
+                {this.state.timerIsRunning ? 
+                    <Icon 
+                        onPress={this.pauseTime}
+                        name="pause"
+                        style={{
+                            fontSize: 60,
+                            color:"green"
+                        }}
+                    />
+                    :
                     <Icon 
                         onPress={this.startTime}
                         name="play"
@@ -48,6 +93,8 @@ class StopWatch extends Component {
                             color:"green"
                         }}
                     />
+                }
+                    
                     <Icon 
                         onPress={this.stopTime}
                         name="md-square"
