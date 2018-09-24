1d065050666a3a75bee60144fee5df92c441a3ec
Christopher Caldwell <clc523@gmail.com>
2018-09-24 07:34:21 -0600

    added workout details screen to show sets for a specific workout

--- screens/WorkoutDetails.js
+++ screens/WorkoutDetails.js
@@ -15,20 +15,35 @@ import { connect } from 'react-redux';
 import { deleteExerciseFromDb, updateWorkoutInDb } from '../Redux/Actions/actionsIndex';
 import { styles } from '../styles/stylesheet';
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, ownProps) => {
+    const currentWorkout = state.Workouts.find(workout => {
+        return workout.id === ownProps.navigation.state.params.id;
+    });
     return {
-        workouts: state.Workouts
+        currentWorkout: currentWorkout
     }
 };
 
 const WorkoutDetails = (props) => {
 
-    const { navigate } = props.navigation;
+    const WorkoutId = props.navigation.state.params.id;
 
     return (
         <Container>
             <Content>
                 <Text>Work out details</Text>
+                {props.currentWorkout.Sets
+                    .map(set =>
+                        <Card key={set.id}>
+                            <CardItem button>
+                                <Text style={{ fontSize: 18 }}>Exercise: {set.ExerciseId}</Text>
+                                <Text style={{ fontSize: 18 }}>Reps: {set.Reps}</Text>
+                                <Text style={{ fontSize: 18 }}>Time: {set.Time}</Text>
+                                <Text style={{ fontSize: 18 }}>Weight: {set.Weight}</Text>
+                            </CardItem>
+                        </Card>
+                    )
+                }
             </Content>
         </Container>
     );
